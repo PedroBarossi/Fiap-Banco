907c0d0dae7d1f2247e5e90decefcc32b2524ef9
Pedro Barossi <pedro3barossi@gmail.com>
2021-12-28 07:37:26 -0300

    Capítulo 4 - Herança
    Adicionada uma subclasse de Conta Corrente à superclasse Conta.

--- src/fiapbanco/Conta.java
+++ src/fiapbanco/Conta.java
@@ -84,4 +84,41 @@ public class Conta {
         return saldo;
     }
     
+    public class ContaCorrente extends Conta {
+        
+        private String tipo;
+        private double chequeEspecial;
+        
+        public ContaCorrente(String tipo){
+            this.tipo = tipo;
+        }
+        
+        public String getTipo(){
+            return tipo;
+        }
+        
+        public void setTipo (String tipo){
+            this.tipo = tipo;
+        }
+        
+        public double getChequeEspecial(){
+            return chequeEspecial;
+        }
+        
+        public void setChequeEspecial(double chequeEspecial){
+            this.chequeEspecial = chequeEspecial;
+        }
+        
+        //comportamentos exclusivos da conta corrente
+        public double getSaldoDisponivel(){
+            return getSaldo() + this.chequeEspecial;
+        }
+        
+        //sobrescrever o método de retirada (adiciona taxa)
+        @Override
+        public void retirar(double valor){
+            valor += 10;
+            super.retirar(valor);
+        }
+    }
 }
